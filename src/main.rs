c1cfc34b8be503f1bcdfec279fb50656705490b6
GLStudios <contact@glstudios.org>
2026-08-28 02:43:09 +1000

    why do you work like this ffmpeg-next

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(pointer_try_cast_aligned)]
+#![feature(pointer_try_cast_aligned, c_variadic)]
 
 use core::panic;
 use std::{
@@ -27,6 +27,8 @@ use ffmpeg_next::{
 };
 use tracing::{
     Level,
+    debug,
+    error,
     trace,
     warn,
 };
@@ -56,125 +58,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing::info!("Hello, world!");
     ffmpeg::init()?;
 
-    // Create wgpu
-
-    // TODO: Support OpenGL & D3D11
-    let (instance, adapter, device, queue) = unsafe {
-        let hal_instance = wgpu_hal::vulkan::Instance::init_with_callback(
-            &wgpu_hal::InstanceDescriptor {
-                name:                     "egui_vulkan",
-                flags:                    wgpu::InstanceFlags::empty(),
-                memory_budget_thresholds: wgpu::MemoryBudgetThresholds::default(),
-                backend_options:          wgpu::BackendOptions::default(),
-                telemetry:                None,
-                display:                  None,
-            },
-            Some(Box::new(|creation_args| {
-                let mut count = 0;
-                let extensions = av_vk_get_optional_instance_extensions(&raw mut count);
-                let extensions = std::slice::from_raw_parts(extensions, count as usize);
-
-                let Ok(binding) = creation_args
-                    .entry
-                    .enumerate_instance_extension_properties(None)
-                else {
-                    return;
-                };
-                let supported_extensions = binding
-                    .iter()
-                    .filter_map(|prop| prop.extension_name_as_c_str().ok())
-                    .collect::<Vec<_>>();
-
-                // Push (unique) ffmpeg extensions
-                for bytes in extensions {
-                    let cstr = CStr::from_ptr(*bytes);
-                    if !creation_args.extensions.contains(&cstr)
-                        && supported_extensions.contains(&cstr)
-                    {
-                        creation_args.extensions.push(cstr);
-                    }
-                }
-
-                // List enabled extensions
-                for extension in creation_args.extensions {
-                    trace!(target: "lowpass_analysis::vulkan_init", ?extension, "Using Instance Extension");
-                }
-            })),
-        )?;
-
-        let selected_exposed = {
-            let exposed_adapters = hal_instance.enumerate_adapters(None);
-
-            let mut idx = 0;
-            for (i, adapter) in exposed_adapters.iter().enumerate() {
-                if adapter.info.device_type == wgpu::DeviceType::DiscreteGpu {
-                    idx = i;
-                }
-            }
-            exposed_adapters.into_iter().nth(idx)
-        }
-        .expect("No valid adapter available");
-        let hal_adapter = &selected_exposed.adapter;
-
-        let hal_device = hal_adapter.open_with_callback(
-            selected_exposed.features,
-            &selected_exposed.capabilities.limits,
-            &wgpu::MemoryHints::default(),
-            Some(Box::new(|creation_args| {
-                let mut count = 0;
-                let extensions = av_vk_get_optional_device_extensions(&raw mut count);
-                let extensions = std::slice::from_raw_parts(extensions, count as usize);
-
-                // Push (unique) ffmpeg extensions
-                for bytes in extensions {
-                    let cstr = CStr::from_ptr(*bytes);
-                    if !creation_args.extensions.contains(&cstr)
-                        && hal_adapter
-                            .physical_device_capabilities()
-                            .supports_extension(cstr)
-                    {
-                        creation_args.extensions.push(cstr);
-                    }
-                }
-
-                // List enabled extensions
-                for extension in creation_args.extensions {
-                    trace!(
-                        target: "lowpass_analysis::vulkan_init",
-                        ?extension,
-                        "Using Instance Extension"
-                    );
-                }
-
-                // Can't see any obvious way to set the features; should be fine
-            })),
-        )?;
-
-        let instance = wgpu::Instance::from_hal::<wgpu_hal::api::Vulkan>(hal_instance);
-        let adapter = instance.create_adapter_from_hal(selected_exposed);
-        let (device, queue) =
-            adapter.create_device_from_hal(hal_device, &wgpu::DeviceDescriptor {
-                label: Some("egui vulkan"),
-                required_limits: wgpu::Limits {
-                    max_texture_dimension_2d: 8192,
-                    ..wgpu::Limits::defaults()
-                },
-                ..Default::default()
-            })?;
-        (instance, adapter, device, queue)
-    };
+    //unsafe {
+    //    ffmpeg::ffi::av_log_set_level(ffmpeg::ffi::AV_LOG_VERBOSE);
+    //}
 
     let options = eframe::NativeOptions {
         renderer: eframe::Renderer::Wgpu,
-        wgpu_options: eframe::WgpuConfiguration {
-            wgpu_setup: egui_wgpu::WgpuSetup::Existing(egui_wgpu::WgpuSetupExisting {
-                instance,
-                adapter,
-                device,
-                queue,
-            }),
-            ..eframe::WgpuConfiguration::default()
-        },
         viewport: ViewportBuilder::default()
             .with_app_id("org.glstudios.lowpass_analysis")
             .with_title("Lowpass Analysis Tool"),
@@ -218,6 +107,7 @@ impl FrameIterator {
             AV_HWDEVICE_TYPE_QSV,
         ]
     };
+    const INITIAL_POOL_SIZE: i32 = 24;
 
     extern "C" fn get_format(
         ctx: *mut ffmpeg::ffi::AVCodecContext,
@@ -229,8 +119,16 @@ impl FrameIterator {
 
             let ctx = ctx.as_mut_unchecked();
 
+            if ctx.sw_pix_fmt == AVPixelFormat::AV_PIX_FMT_NONE {
+                return AVPixelFormat::AV_PIX_FMT_NONE;
+            }
+
             if let Some(hw_device_ctx) = NonNull::new(ctx.hw_device_ctx) {
-                let device_ctx = hw_device_ctx.read().buffer.cast::<AVHWDeviceContext>();
+                let device_ctx = hw_device_ctx
+                    .read()
+                    .data
+                    .try_cast_aligned::<AVHWDeviceContext>()
+                    .expect("FFmpeg didn't align hw_device_ctx");
                 let device_ctx = device_ctx.as_mut_unchecked();
 
                 let ty = device_ctx.type_;
@@ -252,6 +150,7 @@ impl FrameIterator {
                         while p.read() != AVPixelFormat::AV_PIX_FMT_NONE {
                             if p.read() == pixfmt {
                                 trace!(?pixfmt, "Selected pixel format for HW Decoder");
+
                                 return pixfmt;
                             }
                             p = p.add(1);
@@ -261,176 +160,12 @@ impl FrameIterator {
             }
 
             let pixfmt = pix_fmts.read();
-            trace!(?pixfmt, "Selected pixel format for HW Decoder");
+            trace!(?pixfmt, "Selected pixel format for Decoder");
             pixfmt
         }
     }
 
-    extern "C" fn free_device_ctx(ctx: *mut ffmpeg::ffi::AVHWDeviceContext) {
-        unsafe {
-            if !ctx.read().hwctx.is_null() {
-                ffmpeg::ffi::av_free(ctx.read().hwctx);
-            }
-            trace!("Free'd HW device ctx");
-        }
-    }
-
-    fn create_vulkan_hwdevice(
-        render_state: &egui_wgpu::RenderState
-    ) -> Result<*mut ffmpeg::ffi::AVBufferRef, i32> {
-        unsafe {
-            #[allow(clippy::wildcard_imports)]
-            use ffmpeg::ffi::*;
-
-            let mut hw_device_ctx = av_hwdevice_ctx_alloc(AVHWDeviceType::AV_HWDEVICE_TYPE_VULKAN);
-            if hw_device_ctx.is_null() {
-                return Err(ffmpeg::Error::DecoderNotFound.into());
-            }
-            let raw_ctx = hw_device_ctx
-                .read()
-                .data
-                .try_cast_aligned::<AVHWDeviceContext>()
-                .expect("AVHWDeviceContext was not aligned");
-            let ctx_ref = raw_ctx.as_mut_unchecked();
-
-            let hal_instance = render_state
-                .instance
-                .as_hal::<wgpu_hal::api::Vulkan>()
-                .expect("Unable to get Vulkan hal from render_state.instance");
-            let hal_adapter = render_state
-                .adapter
-                .as_hal::<wgpu_hal::api::Vulkan>()
-                .expect("Unable to get Vulkan hal from render_state.adapter");
-            let hal_device = render_state
-                .device
-                .as_hal::<wgpu_hal::api::Vulkan>()
-                .expect("Unable to get Vulkan hal from render_state.adapter");
-
-            let vulkan_proc_addr = hal_instance
-                .shared_instance()
-                .entry()
-                .static_fn()
-                .get_instance_proc_addr;
-
-            let mut device_features = ash::vk::PhysicalDeviceFeatures2::default();
-            hal_device
-                .shared_instance()
-                .raw_instance()
-                .get_physical_device_features2(
-                    hal_device.raw_physical_device(),
-                    &mut device_features,
-                );
-
-            let inst_extensions = hal_instance
-                .shared_instance()
-                .extensions()
-                .iter()
-                .map(|str| str.as_ptr())
-                .collect::<Vec<_>>();
-            let device_extensions = hal_device
-                .enabled_device_extensions()
-                .iter()
-                .map(|str| str.as_ptr())
-                .collect::<Vec<_>>();
-
-            let mut qf = [AVVulkanDeviceQueueFamily {
-                idx:        0,
-                num:        0,
-                flags:      0,
-                video_caps: 0,
-            }; 64]; // magic number moment
-
-            let qf_len = hal_instance
-                .shared_instance()
-                .raw_instance()
-                .get_physical_device_queue_family_properties2_len(hal_device.raw_physical_device());
-            let mut video_props = vec![ash::vk::QueueFamilyVideoPropertiesKHR::default(); qf_len];
-            let mut qf_props = video_props
-                .iter_mut()
-                .map(|vp| ash::vk::QueueFamilyProperties2::default().push_next(vp))
-                .collect::<Vec<_>>();
-            hal_instance
-                .shared_instance()
-                .raw_instance()
-                .get_physical_device_queue_family_properties2(
-                    hal_device.raw_physical_device(),
-                    &mut qf_props,
-                );
-
-            let mut count = 0;
-
-            for (idx, props) in qf_props.iter().enumerate() {
-                qf[count].idx = idx as i32;
-                qf[count].num = props.queue_family_properties.queue_count.cast_signed();
-                qf[count].flags = props.queue_family_properties.queue_flags.as_raw();
-                qf[count].video_caps = 0;
-
-                let p_next = props
-                    .p_next
-                    .cast::<ash::vk::QueueFamilyVideoPropertiesKHR>();
-                if p_next.is_null() {
-                    continue;
-                }
-
-                qf[count].video_caps = p_next.read().video_codec_operations.as_raw().cast_signed();
-
-                count += 1;
-            }
-
-            let device_qf_idx = hal_device.queue_family_index() as usize;
-            if device_qf_idx < count {
-                qf.copy_within(..count, 1);
-                qf[0] = qf[device_qf_idx + 1];
-                count += 1;
-            }
-
-            let hwctx = AVVulkanDeviceContext {
-                alloc: std::ptr::null_mut(),
-                #[allow(clippy::missing_transmute_annotations)] // I'm NOT annotating that
-                get_proc_addr: None,
-                inst: hal_instance
-                    .shared_instance()
-                    .raw_instance()
-                    .handle()
-                    .as_raw() as *mut _,
-                phys_dev: hal_device.raw_physical_device().as_raw() as *mut _,
-                act_dev: hal_device.raw_device().handle().as_raw() as *mut _,
-                device_features: VkPhysicalDeviceFeatures2 {
-                    sType:    device_features.s_type.as_raw(),
-                    pNext:    device_features.p_next, // Hopefully this works
-                    features: std::mem::transmute::<
-                        ash::vk::PhysicalDeviceFeatures,
-                        [u32; 55],
-                    >(device_features.features),
-                },
-                enabled_inst_extensions: inst_extensions.as_ptr().cast(),
-                nb_enabled_inst_extensions: i32::try_from(inst_extensions.len()).expect("More than i32::MAX instance extensions"),
-                enabled_dev_extensions: device_extensions.as_ptr().cast(),
-                nb_enabled_dev_extensions: i32::try_from(device_extensions.len()).expect("More than i32::MAX device extensions"),
-                lock_queue: None,
-                unlock_queue: None,
-                qf,
-                nb_qf: count as i32,
-                queue_flags: 0,
-            };
-            ctx_ref.hwctx = av_malloc(size_of::<AVVulkanDeviceContext>());
-            ctx_ref.hwctx.cast::<AVVulkanDeviceContext>().write(hwctx);
-            ctx_ref.free = Some(Self::free_device_ctx);
-
-            let err = av_hwdevice_ctx_init(hw_device_ctx);
-            if err != 0 {
-                av_buffer_unref(&raw mut hw_device_ctx);
-                Err(err)
-            } else {
-                Ok(hw_device_ctx)
-            }
-        }
-    }
-
-    fn new(
-        mut input: ffmpeg::format::context::Input,
-        render_state: &egui_wgpu::RenderState,
-    ) -> Result<Self, ffmpeg::Error> {
+    fn new(mut input: ffmpeg::format::context::Input) -> Result<Self, ffmpeg::Error> {
         unsafe {
             #[allow(clippy::wildcard_imports)]
             use ffmpeg::ffi::*;
@@ -481,33 +216,16 @@ impl FrameIterator {
 
                 let ctx = ctx.as_mut_ptr().as_mut_unchecked();
 
-                // If hwdevice is same as wgpu hal, use that
-                let err = match ty {
-                    AVHWDeviceType::AV_HWDEVICE_TYPE_VULKAN => {
-                        let res = Self::create_vulkan_hwdevice(render_state);
-                        match res {
-                            Ok(hw_device_ctx) => {
-                                ctx.hw_device_ctx = hw_device_ctx;
-                                0
-                            },
-                            Err(err) => err,
-                        }
-                    },
-                    _ => av_hwdevice_ctx_create(
-                        &raw mut ctx.hw_device_ctx,
-                        ty,
-                        std::ptr::null(),
-                        std::ptr::null_mut(),
-                        0,
-                    ),
-                };
-
+                let err = av_hwdevice_ctx_create(
+                    &raw mut ctx.hw_device_ctx,
+                    ty,
+                    std::ptr::null(),
+                    std::ptr::null_mut(),
+                    0,
+                );
                 if err != 0 {
                     let error = ffmpeg::Error::from(err);
                     warn!(?error, "Error while initializing hwdevice of type {:?}", ty);
-                    if !ctx.hw_device_ctx.is_null() {
-                        av_buffer_unref(&raw mut ctx.hw_device_ctx);
-                    }
                     continue;
                 }
 
@@ -542,7 +260,13 @@ impl Iterator for FrameIterator {
         loop {
             // See if decoder has a frame ready
             match self.decoder.receive_frame(&mut decoded_frame) {
-                Ok(_) => return Some(Ok(decoded_frame.clone())),
+                Ok(()) => {
+                    //unsafe {
+                    //    let ctx = self.decoder.as_ptr().read();
+                    //    trace!(hwaccel = ?ctx.hwaccel, hw_frames = ?ctx.hw_frames_ctx);
+                    //}
+                    return Some(Ok(decoded_frame));
+                },
                 Err(ffmpeg::Error::Other { errno }) if errno == ffmpeg::ffi::EAGAIN => {
                     // Need to feed more packets to the decoder
                 },
@@ -574,7 +298,7 @@ struct App {
     clean:     FrameIterator,
     lowpassed: FrameIterator,
 
-    current: Option<[egui::TextureHandle; 2]>,
+    current: Option<[(); 2]>,
 }
 
 impl App {
@@ -593,6 +317,8 @@ impl App {
             use ffmpeg::ffi::*;
 
             let avframe = frame.as_mut_ptr();
+            let avf = avframe.read();
+            trace!(hwframe = ?avf.hw_frames_ctx);
 
             let pix_desc = pixfmt.descriptor().expect("Invalid Pixel Format");
             let pix_desc = pix_desc.as_ptr().read();
@@ -621,7 +347,10 @@ impl App {
             let vkframe = avframe.read().data[0]
                 .try_cast_aligned::<AVVkFrame>()
                 .expect("AVVkFrame wasn't aligned");
-            assert!(!vkframe.is_null(), "AVVkFrame was null");
+            if vkframe.is_null() {
+                warn!("AVVkFrame was null");
+                return;
+            }
             let vkframe = vkframe.read();
 
             let luma_plane = std::mem::transmute::<u64, ash::vk::Image>(vkframe.img[0]);
@@ -685,7 +414,7 @@ impl App {
         name: &str,
         frame: &mut VideoFrame,
         pixfmt: &ffmpeg::format::Pixel,
-    ) -> egui::TextureHandle {
+    ) {
         unsafe {
             #[allow(clippy::wildcard_imports)]
             use ffmpeg::ffi::*;
@@ -701,6 +430,7 @@ impl App {
                     },
                     _ => unimplemented!("Unsupported HW frame type"),
                 };
+                return;
             } else {
                 // Process as SW frame
                 if pix_desc.flags & AV_PIX_FMT_FLAG_RGB as u64 != 0
@@ -728,7 +458,7 @@ impl App {
             name,
             egui::ImageData::Color(image.into()),
             egui::TextureOptions::NEAREST,
-        )
+        );
     }
 
     #[allow(clippy::missing_const_for_fn)]
@@ -739,13 +469,13 @@ impl App {
         // TODO: Allow this to be done via UI instead
 
         let clean_ctx = ffmpeg::format::input(Self::CLEAN_VIDEO_PATH)?;
-        let mut clean_frames = FrameIterator::new(clean_ctx, render_state)?;
+        let mut clean_frames = FrameIterator::new(clean_ctx)?;
         for _ in 0..24 {
             _ = clean_frames.next();
         }
 
         let dirty_ctx = ffmpeg::format::input(Self::LOWPASSED_VIDEO_PATH)?;
-        let mut dirty_frames = FrameIterator::new(dirty_ctx, render_state)?;
+        let mut dirty_frames = FrameIterator::new(dirty_ctx)?;
 
         // Grab first frame of each
         let frames = match (clean_frames.next(), dirty_frames.next()) {
@@ -826,13 +556,13 @@ impl eframe::App for App {
                 };
 
                 let width = size.x / (frames.len() as f32);
-                for handle in frames {
-                    ui.add(
-                        egui::Image::from_texture(egui::load::SizedTexture::from_handle(handle))
-                            .maintain_aspect_ratio(true)
-                            .fit_to_exact_size(egui::vec2(width, size.y)),
-                    );
-                }
+                //for handle in frames {
+                //    ui.add(
+                //        egui::Image::from_texture(egui::load::SizedTexture::from_handle(handle))
+                //            .maintain_aspect_ratio(true)
+                //            .fit_to_exact_size(egui::vec2(width, size.y)),
+                //    );
+                //}
             });
 
             Some(())
